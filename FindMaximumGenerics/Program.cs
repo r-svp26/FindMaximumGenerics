932b4a16840b669700bb4d42812b7af0e5640253
r-svp26 <riteshk6341@gmail.com>
2021-08-02 17:23:54 +0530

    [Ritesh] Add: Display welcome message

---
+++ FindMaximumGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximumGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Programs.");
+        }
+    }
+}
