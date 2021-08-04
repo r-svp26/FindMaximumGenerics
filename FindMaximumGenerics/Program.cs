d62fadcf0a7e4f3388fad97f21f23902844e5b0a
r-svp26 <riteshk6341@gmail.com>
2021-08-04 21:45:03 +0530

    [Ritesh] Add: Extended the GetMax method to print std out

--- FindMaximumGenerics/Program.cs
+++ FindMaximumGenerics/Program.cs
@@ -10,7 +10,15 @@ namespace FindMaximumGenerics
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Test Program.");
+            int[] arrayInt = { 20, 12, 10, 5 };
+            int actualInt = new Maximum<int>(arrayInt).GetMax();
+            Console.WriteLine("Max value of integers:" + actualInt);
+            float[] arrayFloat = { 25f, 12f, 10f, 5f};
+            float actualFloat = new Maximum<float>(arrayFloat).GetMax();
+            Console.WriteLine("Max value of floats:" + actualFloat);
+            string[] arrayString = { "A", "C", "D", "C" };
+            string actualString = new Maximum<string>(arrayString).GetMax();
+            Console.WriteLine("Max value of string:" + actualString);
         }
     }
 }
